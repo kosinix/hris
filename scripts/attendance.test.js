532b7e6fc4af4593b22b7f7ef69929be76fab3dd
kosinix <kosinix@noreply.github.io>
2021-06-18 10:29:10 +0800

    Test for grace period and unpaid overtime

--- scripts/attendance.test.js
+++ scripts/attendance.test.js
@@ -39,10 +39,11 @@ const db = require('../data/src/db-install');
 
 (async () => {
     try {
+        let employeeId = new db.mongoose.Types.ObjectId("60b22ee0e846b00efc16941a")
         let attendances = [
             {
 
-                "employeeId": new db.mongoose.Types.ObjectId("60b22ee0e846b00efc16941a"),
+                "employeeId": employeeId,
                 "onTravel": false,
                 "logs": [
                     {
@@ -62,12 +63,73 @@ const db = require('../data/src/db-install');
                     },
                     {
                         "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
-                        "dateTime": moment().hour(17).minutes(0).toDate(),
+                        "dateTime": moment().hour(17).minutes(30).toDate(),
                         "mode": 0
                     }
                 ]
+            },
+            {
+
+                "employeeId": employeeId,
+                "onTravel": false,
+                "logs": [
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(8).minutes(16).toDate(),
+                        "mode": 1
+                    },
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(12).minutes(0).toDate(),
+                        "mode": 0
+                    },
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(13).minutes(15).toDate(),
+                        "mode": 1
+                    },
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(17).minutes(0).toDate(),
+                        "mode": 0
+                    }
+                ],
+                createdAt: moment().subtract(1, 'days').toDate(),
+            },
+            {
+
+                "employeeId": employeeId,
+                "onTravel": false,
+                "logs": [
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(8).minutes(0).toDate(),
+                        "mode": 1
+                    },
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(12).minutes(0).toDate(),
+                        "mode": 0
+                    },
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(13).minutes(0).toDate(),
+                        "mode": 1
+                    },
+                    {
+                        "scannerId": new db.mongoose.Types.ObjectId("60c02806cac3cc38e0ccccbf"),
+                        "dateTime": moment().hour(17).minutes(30).toDate(),
+                        "mode": 0
+                    }
+                ],
+                createdAt: moment().subtract(2, 'days').toDate(),
             }
         ]
+        let r = await db.main.Attendance.deleteMany({
+            "employeeId": employeeId,
+        })
+        console.log(`Deleted ${r.deletedCount} "attendance" ...`)
+
         let promises = lodash.map(attendances, (o) => {
             let attendance = new db.main.Attendance(o);
 

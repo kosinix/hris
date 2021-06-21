3a361be8879a8caeba582277a2ef784f73f9f2de
kosinix <kosinix@noreply.github.io>
2021-06-21 10:47:48 +0800

    Add attendance to sidebar and cleaned route

--- data/src/routes/attendance.js
+++ data/src/routes/attendance.js
@@ -19,89 +19,9 @@ let router = express.Router()
 
 router.use('/attendance', middlewares.requireAuthUser )
 
-router.get('/attendance/all', middlewares.guardRoute(['read_all_scanner', 'read_scanner']), async (req, res, next) => {
+router.get('/attendance/all', middlewares.guardRoute(['read_all_attendance', 'read_attendance']), async (req, res, next) => {
     try {
-        let employee = {
-            _id: '60b22ee0e846b00efc16941a',
-            uid: '371170237238',
-        }
-        let payroll = {
-            startDate: '2021-06-01',
-            endDate: '2021-06-30',
-        }
-        //
-        // let date = new Date('2021-06-01 08:00:00')
-        let date = moment('06-01-2021 08:00 AM', 'MM-DD-YYYY hh:mm A')
-        let attendance = new db.main.Attendance({
-            employeeId: employee._id,
-            createdAt: date.clone().toDate(),
-
-            inAM: {
-                dateTime: date.clone().toDate(),
-                confirmed: true,
-            },
-            outAM: date.add(4, 'hours').toDate(),
-            inPM: date.add(1, 'hours').toDate(),
-            outPM: date.add(4, 'hours').toDate(),
-        })
-        await attendance.save()
-
-        date = moment('06-02-2021 08:00 AM', 'MM-DD-YYYY hh:mm A')
-        attendance = new db.main.Attendance({
-            employeeId: employee._id,
-            createdAt: date.clone().toDate(),
-
-            inAM: date.clone().toDate(),
-            outAM: date.add(4, 'hours').toDate(),
-            inPM: date.add(1, 'hours').toDate(),
-            outPM: date.add(4, 'hours').toDate(),
-        })
-        await attendance.save()
-        //
-
-        // All attendances
-        let attendances = await db.main.Attendance.find({
-            employeeId: employee._id,
-            createdAt: {
-                $gte: moment(payroll.startDate).startOf('day').toDate(),
-                $lt: moment(payroll.endDate).endOf('day').toDate(),
-            }
-        })
-
-        res.send(attendances)
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/attendance/dtr', middlewares.guardRoute(['create_scanner']), async (req, res, next) => {
-    try {
-        let employee = {
-            _id: '60b22ee0e846b00efc16941a',
-            uid: '371170237238',
-        }
-
-        // Today attendance
-        let attendance = await db.main.Attendance.findOne({
-            employeeId: employee._id,
-            createdAt: {
-                $gte: moment().startOf('day').toDate(),
-                $lt: moment().endOf('day').toDate(),
-            }
-        })
-
-        console.log(attendance)
-        if(attendance){
-            console.log(moment(attendance.inAM).format('dddd - MMM DD, YYYY hh:mm:ss A'))
-        }
-
-        let qrCodeSvg = qr.imageSync(employee.uid, { size: 6, type: 'svg' })
-
-        res.render('attendance/create.html', {
-            moment: moment,
-            attendance: attendance,
-            qrCodeSvg: qrCodeSvg
-        });
+        res.send('none')
     } catch (err) {
         next(err);
     }

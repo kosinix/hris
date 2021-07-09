d47e8bfd8663233bf1a829b9c64ce70195b7009a
kosinix <kosinix@noreply.github.io>
2021-07-09 20:02:25 +0800

    Updated output

--- data/src/excel-gen.js
+++ data/src/excel-gen.js
@@ -83,6 +83,42 @@ let templateJocos = async (payroll) => {
         bold: true
     };
 
+    sheet.mergeCells('D6:D8');
+    sheet.getCell('D6').value = `POSITION`;
+    sheet.getCell('D6').alignment = { vertical: 'middle', horizontal: 'center' };
+    sheet.getCell('D6').font = {
+        name: 'Arial',
+        size: 11,
+        bold: true
+    };
+
+    sheet.mergeCells('E6:E8');
+    sheet.getCell('E6').value = `DAILY/ \nMONTHLY \nWAGE`;
+    sheet.getCell('E6').alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+    sheet.getCell('E6').font = {
+        name: 'Arial',
+        size: 11,
+        bold: true
+    };
+
+    sheet.mergeCells('F6:K8');
+    sheet.getCell('F6').value = `No. of Days Rendered`;
+    sheet.getCell('F6').alignment = { vertical: 'middle', horizontal: 'center' };
+    sheet.getCell('F6').font = {
+        name: 'Arial',
+        size: 11,
+        bold: true
+    };
+
+    sheet.mergeCells('L6:L8');
+    sheet.getCell('L6').value = `Gross Amount`;
+    sheet.getCell('L6').alignment = { vertical: 'middle', horizontal: 'center' };
+    sheet.getCell('L6').font = {
+        name: 'Arial',
+        size: 11,
+        bold: true
+    };
+
     payroll.employments.forEach((employment, i) => {
         let cellRef = `A${10 + i}`
         sheet.getCell(cellRef).value = i + 1
@@ -104,7 +140,84 @@ let templateJocos = async (payroll) => {
             name: 'Arial',
             size: 14,
         };
+
+        cellRef = `D${10 + i}`
+        sheet.getCell(cellRef).value = `${employment.position}`
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+
+        cellRef = `E${10 + i}`
+        sheet.getCell(cellRef).value = employment.salary
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+
+        cellRef = `F${10 + i}`
+        sheet.getCell(cellRef).value = employment.timeRecord.renderedDays
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+        cellRef = `G${10 + i}`
+        sheet.getCell(cellRef).value = `Days`
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+        cellRef = `H${10 + i}`
+        sheet.getCell(cellRef).value = employment.timeRecord.renderedHours
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+        cellRef = `I${10 + i}`
+        sheet.getCell(cellRef).value = `hours`
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+        cellRef = `J${10 + i}`
+        sheet.getCell(cellRef).value = employment.timeRecord.renderedMinutes
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+        cellRef = `K${10 + i}`
+        sheet.getCell(cellRef).value = `minutes`
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
+        //=F10*E10+H10*E10/8+J10*E10/8/60
+        cellRef = `L${10 + i}`
+        sheet.getCell(cellRef).value = {
+            formula: `F${10 + i}*E${10 + i}+H${10 + i}*E${10 + i}/8+J${10 + i}*E${10 + i}/8/60`,
+            result: parseFloat(employment.amountWorked.toFixed(2))
+        };
+        sheet.getCell(cellRef).font = {
+            name: 'Arial',
+            size: 14,
+        };
     })
+
+    // sheet.columns.forEach(function (column, i) {
+    //     if (![0].includes(i)) {
+    //         var maxLength = 0;
+
+    //         column.eachCell({ includeEmpty: false }, function (cell, rowNumber) {
+    //             if (rowNumber > 5) {
+    //                 var columnLength = cell.value ? cell.value.toString().length : 10;
+    //                 if (columnLength > maxLength) {
+    //                     maxLength = columnLength;
+    //                 }
+    //             }
+    //         });
+    //         column.width = maxLength < 10 ? 10 : maxLength;
+    //     }
+    // });
 
     await workbook.xlsx.writeFile('excel.xlsx');
 }

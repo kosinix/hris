5fc182aeb834197344c41bf98df39104a6c623a3
kosinix <kosinix@noreply.github.io>
2021-11-24 23:23:56 +0800

    Add reports for inc att

--- data/src/routes/reports.js
+++ data/src/routes/reports.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 //// External modules
 const express = require('express')
 const flash = require('kisapmata')
-const phAddress = require('ph-address')
+const lodash = require('lodash')
 const moment = require('moment')
 
 //// Modules
@@ -22,6 +22,175 @@ router.get('/reports/all', async (req, res, next) => {
         
         res.render('reports/all.html', {
             flash: flash.get(req, 'reports'),
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+router.get('/reports/attendance/incomplete', async (req, res, next) => {
+    try {
+
+        let start = lodash.get(req, 'query.start', moment().format('YYYY-MM-DD'))
+        let end = lodash.get(req, 'query.end', moment().format('YYYY-MM-DD'))
+
+        let startMoment = moment(start).startOf('day')
+        let endMoment = moment(end).endOf('day')
+        let aggr = []
+        aggr.push({
+            $addFields: {
+                "logsLength": {
+                    $size: '$logs'
+                }
+            }
+        })
+
+        aggr.push({
+            $match: {
+                type: 'normal',
+                logsLength: {
+                    $lt: 4
+                },
+                createdAt: {
+                    $gte: startMoment.clone().startOf('day').toDate(),
+                    $lte: endMoment.clone().endOf('day').toDate(),
+                }
+            }
+        })
+
+        // Join
+        aggr.push({
+            $lookup: {
+                localField: 'employeeId',
+                foreignField: '_id',
+                from: 'employees',
+                as: 'employees'
+            }
+        })
+
+        aggr.push({
+            $lookup: {
+                localField: 'employmentId',
+                foreignField: '_id',
+                from: 'employments',
+                as: 'employments'
+            }
+        })
+
+        aggr.push({
+            $addFields: {
+                "employee": {
+                    $arrayElemAt: ["$employees", 0]
+                }
+            }
+        })
+
+        aggr.push({
+            $addFields: {
+                "employment": {
+                    $arrayElemAt: ["$employments", 0]
+                }
+            }
+        })
+
+        
+        aggr.push({
+            $project: {
+                employees: 0,
+                employments: 0,
+            }
+        })
+
+        let attendances = await db.main.Attendance.aggregate(aggr)
+        // return res.send(attendances)
+        res.render('reports/attendance/incomplete.html', {
+            flash: flash.get(req, 'reports'),
+            attendances: attendances,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/reports/attendance/missing', async (req, res, next) => {
+    try {
+
+        let start = lodash.get(req, 'query.start', moment().format('YYYY-MM-DD'))
+        let end = lodash.get(req, 'query.end', moment().format('YYYY-MM-DD'))
+
+        let startMoment = moment(start).startOf('day')
+        let endMoment = moment(end).endOf('day')
+        let aggr = []
+        aggr.push({
+            $addFields: {
+                "logsLength": {
+                    $size: '$logs'
+                }
+            }
+        })
+
+        aggr.push({
+            $match: {
+                type: 'normal',
+                logsLength: {
+                    $lt: 4
+                },
+                createdAt: {
+                    $gte: startMoment.clone().startOf('day').toDate(),
+                    $lte: endMoment.clone().endOf('day').toDate(),
+                }
+            }
+        })
+
+        // Join
+        aggr.push({
+            $lookup: {
+                localField: 'employeeId',
+                foreignField: '_id',
+                from: 'employees',
+                as: 'employees'
+            }
+        })
+
+        aggr.push({
+            $lookup: {
+                localField: 'employmentId',
+                foreignField: '_id',
+                from: 'employments',
+                as: 'employments'
+            }
+        })
+
+        aggr.push({
+            $addFields: {
+                "employee": {
+                    $arrayElemAt: ["$employees", 0]
+                }
+            }
+        })
+
+        aggr.push({
+            $addFields: {
+                "employment": {
+                    $arrayElemAt: ["$employments", 0]
+                }
+            }
+        })
+
+        
+        aggr.push({
+            $project: {
+                employees: 0,
+                employments: 0,
+            }
+        })
+
+        let attendances = await db.main.Attendance.aggregate(aggr)
+        // return res.send(attendances)
+        res.render('reports/attendance/incomplete.html', {
+            flash: flash.get(req, 'reports'),
+            attendances: attendances,
         });
     } catch (err) {
         next(err);

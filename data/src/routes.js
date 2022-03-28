a257b1bd015aa21d8680bdab3e4fa617c01f8c85
kosinix <kosinix@noreply.github.io>
2022-03-28 13:52:12 +0800

    Add online services to admin side

--- data/src/routes.js
+++ data/src/routes.js
@@ -15,6 +15,7 @@ router.use(require('./routes/protected'));
 router.use(require('./routes/hdf'));
 router.use(require('./routes/e-profile'));
 router.use(require('./routes/hros'));
+router.use(require('./routes/online-services'));
 router.use(require('./routes/employee'));
 router.use(require('./routes/memo'));
 router.use(require('./routes/payroll'));

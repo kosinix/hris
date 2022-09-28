46909780b937dd1e657618298357b536ef34950b
kosinix <kosinix@users.noreply.github.com>
2022-09-28 11:12:52 +0800

    Change to 15 min increments in backend

--- data/src/routes/schedule.js
+++ data/src/routes/schedule.js
@@ -145,11 +145,11 @@ router.post('/schedule/create', middlewares.guardRoute(['create_schedule']), asy
                 }
 
                 // segment here contains time in HH:mm format from HTML input
-                if (start % 30 > 0) {
-                    errors.push('Start Time must be in 30-minute increments.')
+                if (start % 15 > 0) {
+                    errors.push('Start Time must be in 15-minute increments.')
                 }
-                if (end % 30 > 0) {
-                    errors.push('End Time must be in 30-minute increments.')
+                if (end % 15 > 0) {
+                    errors.push('End Time must be in 15-minute increments.')
                 }
 
                 if (end <= start) {
@@ -335,11 +335,11 @@ router.post('/schedule/:scheduleId', middlewares.guardRoute(['update_schedule'])
                 }
 
                 // segment here contains time in HH:mm format from HTML input
-                if (start % 30 > 0) {
-                    errors.push('Start Time must be in 30-minute increments.')
+                if (start % 15 > 0) {
+                    errors.push('Start Time must be in 15-minute increments.')
                 }
-                if (end % 30 > 0) {
-                    errors.push('End Time must be in 30-minute increments.')
+                if (end % 15 > 0) {
+                    errors.push('End Time must be in 15-minute increments.')
                 }
 
                 if (end <= start) {

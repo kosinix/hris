24ff411f03229af56db3a0236e699a28510254ab
kosinix <kosinix@users.noreply.github.com>
2022-12-02 11:49:18 +0800

    Add flag lowering to employee side

--- data/src/routes/hros.js
+++ data/src/routes/hros.js
@@ -441,7 +441,7 @@ router.get('/hros/flag/all', middlewares.guardRoute(['use_employee_profile']), m
 
 
         let query = {
-            createdAt: {
+            dateTime: {
                 $gte: momentDate.clone().startOf('day').toDate(),
                 $lte: momentDate.clone().endOf('day').toDate(),
             }
@@ -549,29 +549,6 @@ router.get('/hros/flag/create', middlewares.guardRoute(['use_employee_profile'])
         res.render('hros/flag-raising/create.html', data)
     } catch (err) {
         next(err);
-    }
-});
-router.post('/hros/flag/location', middlewares.guardRoute(['use_employee_profile']), middlewares.requireAssocEmployee, async (req, res, next) => {
-    try {
-        let body = req.body
-
-        let found = await req.app.locals.db.main.Map.findOne({
-            geo: {
-                $geoIntersects: {
-                    $geometry: {
-                        "type": "Point",
-                        "coordinates": body.coordinates
-                    }
-                }
-            }
-        }).lean()
-
-        if (!found) {
-            return res.status(404).send('')
-        }
-        res.send(found.name)
-    } catch (err) {
-        next(new AppError(err.message));
     }
 });
 router.post('/hros/flag/log', middlewares.guardRoute(['use_employee_profile']), middlewares.requireAssocEmployee, middlewares.isFlagRaisingDay, async (req, res, next) => {
@@ -688,7 +665,7 @@ router.post('/hros/flag/log', middlewares.guardRoute(['use_employee_profile']), 
         })
 
         aggr.push({
-            $sort: { createdAt: 1 }
+            $sort: { dateTime: 1 }
         })
 
         //console.log(aggr)
@@ -711,6 +688,292 @@ router.post('/hros/flag/log', middlewares.guardRoute(['use_employee_profile']), 
 
         flash.ok(req, 'attendance', 'Flag raising attendance saved.')
         res.send(attendance)
+    } catch (err) {
+        next(new AppError(err.message));
+    }
+});
+
+// Flag lowering
+router.get('/hros/flag-lowering/all', middlewares.guardRoute(['use_employee_profile']), middlewares.requireAssocEmployee, async (req, res, next) => {
+    try {
+        let employee = res.employee.toObject()
+
+        // let date = lodash.get(req, 'query.date', moment().format('YYYY-MM-DD'))
+        let momentDate = moment()
+
+
+        let query = {
+            dateTime: {
+                $gte: momentDate.clone().startOf('day').toDate(),
+                $lte: momentDate.clone().endOf('day').toDate(),
+            }
+        }
+
+        let aggr = []
+        aggr.push({ $match: query })
+        aggr.push({
+            $lookup: {
+                from: "employees",
+                localField: "employeeId",
+                foreignField: "_id",
+                as: "employees"
+            }
+        })
+        aggr.push({
+            $addFields: {
+                "employee": {
+                    $arrayElemAt: ["$employees", 0]
+                },
+            }
+        })
+        // Turn array employees into field employee
+        // Add field employee
+        aggr.push({
+            $project: {
+                employees: 0,
+            }
+        })
+
+        // Hide not needed for lighter payload
+        aggr.push({
+            $project: {
+                employee: {
+                    addresses: 0,
+                    personal: 0,
+                    employments: 0,
+                    mobileNumber: 0,
+                    phoneNumber: 0,
+                    documents: 0,
+                    createdAt: 0,
+                    updatedAt: 0,
+                    uuid: 0,
+                    uid: 0,
+                    group: 0,
+                    __v: 0,
+                    profilePhoto: 0,
+                    acceptedDataPrivacy: 0,
+                    birthDate: 0,
+                    civilStatus: 0,
+                    addressPermanent: 0,
+                    addressPresent: 0,
+                    email: 0,
+                    history: 0,
+                    speechSynthesisName: 0,
+                    address: 0
+                }
+            }
+        })
+
+        //console.log(aggr)
+        let alreadyLogged = false
+
+        let attendances = await req.app.locals.db.main.AttendanceFlagLowering.aggregate(aggr)
+        attendances = attendances.map(attendance => {
+            if (!attendance.source.photo) {
+                attendance.source.photo = lodash.get(attendance, 'extra.photo', '')
+            }
+            attendance.logTime = moment(attendance.dateTime).format('hh:mm A')
+
+            if (lodash.invoke(employee, '_id.toString') === lodash.invoke(attendance, 'employeeId.toString')) {
+                alreadyLogged = true
+            }
+            attendance = lodash.pickBy(attendance, function (a, key) {
+                return ['_id', 'employeeId', 'logTime', 'source', 'employee'].includes(key)
+            });
+            return attendance
+        })
+        // return res.send(attendances)
+
+        res.render('hros/flag-lowering/all.html', {
+            flash: flash.get(req, 'attendance'),
+            momentDate: momentDate,
+            attendances: attendances,
+            alreadyLogged: alreadyLogged,
+            s3Prefix: `/${CONFIG.aws.bucket1.name}/${CONFIG.aws.bucket1.prefix}`,
+            serverUrl: CONFIG.app.url,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+router.get('/hros/flag-lowering/create', middlewares.guardRoute(['use_employee_profile']), middlewares.requireAssocEmployee, middlewares.isFlagLoweringDay, async (req, res, next) => {
+    try {
+        let user = res.user.toObject()
+        let employee = res.employee.toObject()
+
+        let data = {
+            title: 'Human Resource Online Services (HROS) - Flag',
+            flash: flash.get(req, 'hros'),
+            user: user,
+            employee: employee,
+            momentNow: moment(),
+        }
+        res.render('hros/flag-lowering/create.html', data)
+    } catch (err) {
+        next(err);
+    }
+});
+router.post('/hros/flag-lowering/log', middlewares.guardRoute(['use_employee_profile']), middlewares.requireAssocEmployee, middlewares.isFlagLoweringDay, async (req, res, next) => {
+    try {
+        let employee = res.employee.toObject()
+
+        // return res.send(req.body)
+        let body = req.body
+
+        let lat = body.lat
+        let lon = body.lon
+        let webcamPhoto = body.webcamPhoto
+        let campus = body.campus
+
+        // Photo
+        let saveList = null
+        if (webcamPhoto) {
+            let file = uploader.toReqFile(webcamPhoto)
+            let files = {
+                photos: [file]
+            }
+            let localFiles = await uploader.handleExpressUploadLocalAsync(files, CONFIG.app.dirs.upload)
+            let imageVariants = await uploader.resizeImagesAsync(localFiles, null, CONFIG.app.dirs.upload); // Resize uploaded images
+
+            let uploadList = uploader.generateUploadList(imageVariants, localFiles)
+            saveList = uploader.generateSaveList(imageVariants, localFiles)
+            await uploader.uploadToS3Async(uploadList)
+            await uploader.deleteUploadsAsync(localFiles, imageVariants)
+            req.localFiles = localFiles
+            req.imageVariants = imageVariants
+            req.saveList = saveList
+
+            // console.log(uploadList, saveList)
+        }
+
+        let momentDate = moment()
+
+        // Log
+        let attendance = await req.app.locals.db.main.AttendanceFlagLowering.create({
+            employeeId: employee._id,
+            dateTime: momentDate.toDate(),
+            type: 'normal',
+            source: {
+                id: res.user._id,
+                type: 'userAccount', // Online user account
+                lat: lat,
+                lon: lon,
+                campus: campus,
+                photo: lodash.get(saveList, 'photos[0]', ''),
+            }
+        })
+
+        // 
+        let query = {
+            dateTime: {
+                $gte: momentDate.clone().startOf('day').toDate(),
+                $lte: momentDate.clone().endOf('day').toDate(),
+            }
+        }
+
+        let aggr = []
+        aggr.push({ $match: query })
+        aggr.push({
+            $lookup: {
+                from: "employees",
+                localField: "employeeId",
+                foreignField: "_id",
+                as: "employees"
+            }
+        })
+        aggr.push({
+            $addFields: {
+                "employee": {
+                    $arrayElemAt: ["$employees", 0]
+                },
+            }
+        })
+        // Turn array employees into field employee
+        // Add field employee
+        aggr.push({
+            $project: {
+                employees: 0,
+            }
+        })
+
+        // Hide not needed for lighter payload
+        aggr.push({
+            $project: {
+                employee: {
+                    addresses: 0,
+                    personal: 0,
+                    employments: 0,
+                    mobileNumber: 0,
+                    phoneNumber: 0,
+                    documents: 0,
+                    createdAt: 0,
+                    updatedAt: 0,
+                    uuid: 0,
+                    uid: 0,
+                    group: 0,
+                    __v: 0,
+                    profilePhoto: 0,
+                    acceptedDataPrivacy: 0,
+                    birthDate: 0,
+                    civilStatus: 0,
+                    addressPermanent: 0,
+                    addressPresent: 0,
+                    email: 0,
+                    history: 0,
+                    speechSynthesisName: 0,
+                    address: 0
+                }
+            }
+        })
+
+        aggr.push({
+            $sort: { dateTime: 1 }
+        })
+
+        //console.log(aggr)
+        let attendances = await req.app.locals.db.main.AttendanceFlagLowering.aggregate(aggr)
+        attendances = attendances.map(attendance => {
+            if (!attendance.source.photo) {
+                attendance.source.photo = lodash.get(attendance, 'extra.photo', '')
+            }
+            attendance.logTime = moment(attendance.dateTime).format('hh:mm A')
+
+            attendance = lodash.pickBy(attendance, function (a, key) {
+                return ['_id', 'employeeId', 'logTime', 'source', 'employee'].includes(key)
+            });
+            return attendance
+        })
+        //return res.send(attendances)
+
+        let room = momentDate.format('YYYY-MM-DD')
+        req.app.locals.io.of("/flag-lowering").to(room).emit('added', attendances.pop())
+
+        flash.ok(req, 'attendance', 'Attendance added.')
+        res.send(attendance)
+    } catch (err) {
+        next(new AppError(err.message));
+    }
+});
+
+router.post('/hros/flag/location', middlewares.guardRoute(['use_employee_profile']), middlewares.requireAssocEmployee, async (req, res, next) => {
+    try {
+        let body = req.body
+
+        let found = await req.app.locals.db.main.Map.findOne({
+            geo: {
+                $geoIntersects: {
+                    $geometry: {
+                        "type": "Point",
+                        "coordinates": body.coordinates
+                    }
+                }
+            }
+        }).lean()
+
+        if (!found) {
+            return res.status(404).send('')
+        }
+        res.send(found.name)
     } catch (err) {
         next(new AppError(err.message));
     }

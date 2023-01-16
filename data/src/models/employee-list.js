227efb04606e022223e8d77a0c10efe01373d120
kosinix <kosinix@users.noreply.github.com>
2023-01-16 09:23:44 +0800

    Add payroll groups
    # Conflicts:
    #	data/src/routes/payroll.js
    #	data/view/parts/sidebar.html
    #	data/view/payroll/group/all.html
    #	data/view/payroll/group/read.html

--- data/src/models/employee-list.js
+++ data/src/models/employee-list.js
@@ -24,6 +24,7 @@ const schema = new Schema({
             lastName: String,
             suffix: String,
             position: String,
+            fundSource: String,
         }
     ],
     tags: [],

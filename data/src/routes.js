6d6b1add0ac3041e3104ee2838797e90017eb78c
kosinix <kosinix@users.noreply.github.com>
2023-07-16 20:52:19 +0800

    Move clinic to its own section

--- data/src/routes.js
+++ data/src/routes.js
@@ -14,6 +14,7 @@ router.use(require('./routes/register'));
 router.use(require('./routes/registration'));
 router.use(require('./routes/protected'));
 router.use(require('./routes/hdf'));
+router.use(require('./routes/clinic'));
 router.use(require('./routes/e-profile'));
 router.use(require('./routes/hros'));
 router.use(require('./routes/online-services'));

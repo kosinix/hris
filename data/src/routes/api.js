f4f8a0c0211a7c4e1867ba3bbd72bc9b3267b25e
kosinix <kosinix@users.noreply.github.com>
2023-07-19 13:21:15 +0800

    Move rate limit to login only

--- data/src/routes/api.js
+++ data/src/routes/api.js
@@ -30,7 +30,7 @@ const uploader = require('../uploader')
 // Router
 let router = express.Router()
 
-router.use('/api', middlewares.api.rateLimit)
+// router.use('/api', )
 
 // Status
 router.get('/api/status', async (req, res, next) => {
@@ -42,7 +42,7 @@ router.get('/api/status', async (req, res, next) => {
 })
 
 // Public API
-router.post('/api/login', async (req, res, next) => {
+router.post('/api/login', middlewares.api.rateLimit, async (req, res, next) => {
     try {
 
         let post = req.body

369e6ab6cd6aa0af2795526390ea5f7c088c764c
kosinix <kosinix@users.noreply.github.com>
2023-08-11 09:40:48 +0800

    Add comment

--- data/src/routes/pds.js
+++ data/src/routes/pds.js
@@ -33,7 +33,7 @@ const noCaps = (val) => {
                     return v
                 }
             }
-            if(/^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(arr.join(''))){
+            if(/^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(arr.join(''))){ // Roman Numerals
                 return v
             }
             return v.toLowerCase()
